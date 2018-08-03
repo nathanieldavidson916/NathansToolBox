004bd3fbb68dd982bd0d0e43d9a514ec2efe552e
Nathan <nathanieldavidson916@yahoo.com>
2018-08-03 12:22:14 -0700

    [documentation]

--- ExportSelection.py
+++ ExportSelection.py
@@ -1,3 +1,6 @@
+## Author: Nathan Davidson
+## Github: https://www.github.com/nathanieldavidson916
+
 import pymel.core as pm
 pm.loadPlugin("fbxmaya") # LOAD PLUGIN
 
@@ -16,10 +19,11 @@ x.ExportLocation()
 
 for item in x.files:
     FilePath = item
+    print(item)
     FileName = item.split('/')[-1:][0].split('.')[0]
     
     cmds.file(FilePath, i=True, namespace=FileName)
-    
+
 Objects = cmds.ls(type="mesh")
 for object in Objects:
     exportName = object.split(':')[0]
